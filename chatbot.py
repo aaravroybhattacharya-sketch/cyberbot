3564ccfd2b3020cc420541f51bd8b379fe702295
aaravroybhattacharya-sketch <aaravroybhattacharya@gmail.com>
2026-09-10 17:50:17 +0530

    Refactor chatbot.py to implement Streamlit interface
    Removed PDF content and added Streamlit app layout with chat functionality.

--- chatbot.py
+++ chatbot.py
@@ -1,149 +1,215 @@
-%PDF-1.4
-%“Œ‹ž ReportLab Generated PDF document http://www.reportlab.com
-1 0 obj
-<<
-/F1 2 0 R /F2 3 0 R /F3 4 0 R /F4 5 0 R /F5 9 0 R
->>
-endobj
-2 0 obj
-<<
-/BaseFont /Helvetica /Encoding /WinAnsiEncoding /Name /F1 /Subtype /Type1 /Type /Font
->>
-endobj
-3 0 obj
-<<
-/BaseFont /Helvetica-Bold /Encoding /WinAnsiEncoding /Name /F2 /Subtype /Type1 /Type /Font
->>
-endobj
-4 0 obj
-<<
-/BaseFont /Courier /Encoding /WinAnsiEncoding /Name /F3 /Subtype /Type1 /Type /Font
->>
-endobj
-5 0 obj
-<<
-/BaseFont /ZapfDingbats /Name /F4 /Subtype /Type1 /Type /Font
->>
-endobj
-6 0 obj
-<<
-/Contents 14 0 R /MediaBox [ 0 0 612 792 ] /Parent 13 0 R /Resources <<
-/Font 1 0 R /ProcSet [ /PDF /Text /ImageB /ImageC /ImageI ]
->> /Rotate 0 /Trans <<
+import streamlit as st
+from groq import Groq  # Free high-performance cloud AI host
 
->> 
-  /Type /Page
->>
-endobj
-7 0 obj
-<<
-/Contents 15 0 R /MediaBox [ 0 0 612 792 ] /Parent 13 0 R /Resources <<
-/Font 1 0 R /ProcSet [ /PDF /Text /ImageB /ImageC /ImageI ]
->> /Rotate 0 /Trans <<
+# 1. PREMIUM APPARATUS LAYOUT
+st.set_page_config(
+    page_title="NEO-NET GLOBAL CORE // v6.0", 
+    page_icon="🔮", 
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
 
->> 
-  /Type /Page
->>
-endobj
-8 0 obj
-<<
-/Contents 16 0 R /MediaBox [ 0 0 612 792 ] /Parent 13 0 R /Resources <<
-/Font 1 0 R /ProcSet [ /PDF /Text /ImageB /ImageC /ImageI ]
->> /Rotate 0 /Trans <<
+# 2. CHATGPT-STYLE COMPACT BAR AND MULTI-THEME CSS
+st.markdown("""
+    <style>
+        .stApp { background-color: #0d0f12 !important; color: #ffffff !important; }
+        
+        /* Remove the native Streamlit sidebar hamburger menu and headers */
+        section[data-testid="stSidebar"] { display: none !important; }
+        footer {visibility: hidden;}
+        
+        .glow-title { color: #00f0ff; text-shadow: 0 0 12px rgba(0, 240, 255, 0.6); font-family: 'Courier New', monospace; font-weight: bold; font-size: 2.5rem; }
+        
+        /* Chat bubble styles */
+        div[data-testid="stChatMessage"] { background-color: #13171f !important; border-left: 4px solid #00f0ff !important; border-radius: 4px 12px 12px 4px !important; margin-bottom: 15px !important; padding: 20px !important; }
+        div[data-testid="stChatMessage"] p, div[data-testid="stChatMessage"] span, div[data-testid="stChatMessage"] li { color: #ffffff !important; font-size: 16px !important; }
+        div[data-testid="stChatMessage"]:has(div[data-testid="stChatMessageAvatar"]):nth-child(even) { border-left: 4px solid #bd00ff !important; background-color: #17131f !important; }
+        div[data-testid="stChatMessage"]:has(div[data-testid="stChatMessageAvatar"]):nth-child(even) p, div[data-testid="stChatMessage"]:has(div[data-testid="stChatMessageAvatar"]):nth-child(even) span { color: #00f0ff !important; }
+        
+        /* 🎨 DARK BORDERLESS ATTRACTIVE CHATBAR OVERRIDES */
+        div[data-testid="stChatInput"] {
+            background-color: #1a1f26 !important;
+            border: none !important;
+            border-radius: 24px !important;
+            box-shadow: none !important;
+            padding: 4px 12px !important;
+        }
+        
+        div[data-testid="stChatInput"] textarea {
+            color: #ffffff !important; 
+            background-color: transparent !important;
+            border: none !important;
+            box-shadow: none !important;
+        }
+        
+        /* ➕ MINIMALIST PLUS BUTTON OVERRIDES */
+        .stFileUploader button {
+            background: transparent !important;
+            border: none !important;
+            color: #ffffff !important;
+            font-size: 28px !important;
+            font-weight: bold !important;
+            padding: 0 !important;
+            margin: 0 !important;
+            width: auto !important;
+            height: auto !important;
+            box-shadow: none !important;
+        }
+        
+        /* 🛠️ Comprehensive Fix: Scrub upload icons, drag-drop text boxes, and file limits completely */
+        div[data-testid="stFileUploaderDropzone"] {
+            padding: 0 !important;
+            border: none !important;
+            background: transparent !important;
+            min-height: unset !important;
+        }
+        div[data-testid="stFileUploaderDropzone"] svg {
+            display: none !important; /* Force hide the upload icon arrow */
+        }
+        div[data-testid="stFileUploaderDropzone"] div {
+            display: none !important; /* Force hide placeholder words like 'Upload' */
+        }
+        div[data-testid="stFileUploaderFileData"] {
+            display: none !important; /* Hide file data elements underneath */
+        }
+        .stFileUploader small {
+            display: none !important; /* Hide the 200MB text string */
+        }
+        .stFileUploader label {
+            display: none !important; /* Hide label layout items */
+        }
+        .stFileUploader {
+            padding-top: 8px !important;
+            display: flex !important;
+            justify-content: center !important;
+            align-items: center !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
->> 
-  /Type /Page
->>
-endobj
-9 0 obj
-<<
-/BaseFont /Helvetica-Oblique /Encoding /WinAnsiEncoding /Name /F5 /Subtype /Type1 /Type /Font
->>
-endobj
-10 0 obj
-<<
-/Contents 17 0 R /MediaBox [ 0 0 612 792 ] /Parent 13 0 R /Resources <<
-/Font 1 0 R /ProcSet [ /PDF /Text /ImageB /ImageC /ImageI ]
->> /Rotate 0 /Trans <<
+# Check if the app is being run locally by you or via the public web URL
+is_local_user = st.context.headers.get("Host", "").startswith("localhost") or st.context.headers.get("Host", "").startswith("127.0.0.1")
 
->> 
-  /Type /Page
->>
-endobj
-11 0 obj
-<<
-/PageMode /UseNone /Pages 13 0 R /Type /Catalog
->>
-endobj
-12 0 obj
-<<
-/Author (\(anonymous\)) /CreationDate (D:20260910120511+00'00') /Creator (\(unspecified\)) /Keywords () /ModDate (D:20260910120511+00'00') /Producer (ReportLab PDF Library - www.reportlab.com) 
-  /Subject (\(unspecified\)) /Title (NEO-NET GLOBAL CORE CHATBOT CODE) /Trapped /False
->>
-endobj
-13 0 obj
-<<
-/Count 4 /Kids [ 6 0 R 7 0 R 8 0 R 10 0 R ] /Type /Pages
->>
-endobj
-14 0 obj
-<<
-/Filter [ /ASCII85Decode /FlateDecode ] /Length 1707
->>
-stream
-GauHKs)6UN'`Yjr`B0sGkX9+$..-6kUpNiTFAOqf-V\*N*9%Oe&j*E?g2=M%rLO,QTMV;jm/dX2Ic(&eTD.\3@,o9Vm\:KSb_LgD#Fl_-&NKGu*W.g?rn`u)B-pcb_,9S:KlP=klC5BF*&V\?NqZB'RG1(:.GCdfbr3"F2.NV5j`R[Uc\K!sm[;gao5<H<K*.JtW]'jenBTg?[h&IFi4h$bs2<6Hlc*p]lFL/Ol$(*bA*OmI;)i`rNr>=o#fS2f.G$6d>SG*lka95?8es-"67,-gO#.O*P*CXoTLP)U)Bp?`?K;_#+[[^mWmDHBT.SPf;=?UoqJFQ6;'J!%JfiHs-'T'pMJ.PRSP,tJZ&i/*]k)X9'8dZGN/p7;HrBI]Y1eXGYcD[P5?0s_M=KYi,X<#H6lrU$A%\`p/5)FhM0[=O7$%sXW_#`<!Q)a.#QgG39.6`nq\:mnN,(IDQPd@NT.)_jPb^R@`3mT5Or1g?P%5GqWk&M"qN4))f:Je*Vj(jOg*;cRLpMgO%;/.hf5P!1@i+X=1_Wd5_FU$uK"9>$77:%L0UC]+bQQglB6XHDP#@+h34:Nd_ET,>"%42^gO`Q*CSWZV!h:b4qHg(Bh#Y":F8N+i4b1!<TmQ1Qh'lW0<8V4s9Q-"B&K;8(o1?`uR*eSToX(uVd\R;PW\Ll*=t7N&0'c/5i3UhmgcW##jj\#/(-,?o/57jZKHeNtTiXM?NREXQBk\iUKOfspV7H5;s&!*;<B,KkqT*/^i.#Ee\<'J34nmpZm=Tb.!M/Pbhm30(?3fWtK>hAt=:Cn5jhl,<<*BXm2l5sVqYS^.\Z/96=U*b++J[6gCsGBUUD1!?0C7&GO<qY8TdF$:^r',N>YA"FYLm/!3-YUT;_#.&GE.rCO+TICB$!BnVa2rU<+7$@]Vq#2gApdd?6-*a<OCs(+&0RSROZS!dur.3WNjl!>[,9KL0uli@pdk$F<h"?211lI`<(d&Tj#[WOReSU<`g@iimoRbM14WEZm.Yi:-d8iD5imF7*TEhf!7B=b^^,FFp2W'F#tr,:1Le`K!B9373lFN#4_7(4!2AeY3>8r$D`7]Q&uZLM;;&\52F0.R"Vdh`P0bIo'$__e#rJ5((*>K"Lru612CqPZH;-S)oW:FA?;jVkA0kR]QT=*A^X6k\WtMI6a'uTPXnpOOKir&P(YAf;rJpK`Qe<b'ZNHV1N!@_CQUNk3IEZUOYF_nMV56Vc)ddVlXI3CFr>sjmi[q#"kp%,Z3=L(`cjr0N/8I=2bU[pSAI$"mPh\:X\m<*E*/tNb(oiujsL-;6@/mO22jL]:2F\j5CF8.i:l%k%/qatiBQt\B5CXgiV*;%ZYof[laORD!*nFMj$qICmhIpCfh]3J]md_=i=2AUjXm(cg=5nTK"9I+9UlAaK+!TCY?=\9[7P9JSp,h@UB^4@cJ?/1mc7p8-0EHaPK"(4DTSj1*sh:h:S`/$?e>qt/I=oo8ooO]_/ML][6L8IS^pfAj0i.o+*qcd7\3o_IeRS5]CFs'aV&o**8,=9%<C5Spf:Gq`$@O+moo8#4oT[2qQCkPG3?WR<HPh2mNRLcAK<^\jB?AbNG$Z,,lGDsn-K`!M_pVSVHfO!m3TA[_I+Y\i8Lfb]Pf4RC*X[$TbLX,mNu\KVD!Ss8kJOUO><r"$Ybo'9p>Gp47aY_0@9PsbeTE:qAI(d@rUrb$@DdQ_1^c;~>endstream
-endobj
-15 0 obj
-<<
-/Filter [ /ASCII85Decode /FlateDecode ] /Length 1872
->>
-stream
-Gatm;Irj='&H+hc@dD<6IU"QMV/8glAgefOF+g@IFtO";7bGRYOX4(l8SG$ZrDnXa%*:BK'kSFpYBHO]hnG@KY_V+6G4f?q%iuE\8l44HDZloB>6_87%<(:]7VQaChM9_6G2nVk77U7p`#'Ct&4OsaIT<n/?:kUGDr.1o(u;oXK?WYQosk6!T-<aFTouA\.UWr&3['@f"#!cH87dT2n$g.1$JnO,?-G\[U+D6s"]D&YJ$8XXFsg[]?9eoJ(oe#QWfAtbN76)ZQR&6h=NTX9'\'Yp4UL)f]bggJ'2GD>&88To+,W.!,*bqI@K?WG;+NL`^jIS]-mc'*VtaJ)U?2E.M+0[g,Y,Ug/QJHpZS@0Y=g60.!ndK9`lAA[).5[4Qm6\R2?ZiOA)rM:!ha<pM2<8Yd`mCG*>?_j%EOiZIrFfB!uQ>o5T@[1pcK[h%-dA7bZ:Ku9FW[CA,SM(pW$'#&E(P87OAj088`n0U'na]:4I-Z2+>6<V?LcIK12nmO&lJUX*'*%:_^4k5&*f@qhIUWVOS\ZDE<faCB]ENkQ"0tY6U2HH.>hBm;LlcAX5!C*`7CqCm6Z),RH.66Y_O_;?:&!k[4b8.:>G,.[VoQR3q0[1EWda&3b;AMpP-.Z8I86iuY3k,WAB9NH$1X$_uO>Rf8WE;/1_3*N-Z3V'F.HCM"c<O=cO[e,HcbpiI%CrO;29IS7u?DI9s$CcB%>Xk!?I!QWIQ7B:S<oih5cXh33f7Z<0B#P)&k\&5-I4ssN82a^8M_(`dkjt,3)PB(OhM##kl;:09O(YT</:0Zm'C+UhL&VSd"SCHh`_dZS+H:5/A5p_WC]Xg,?g_<4'AfOA\9-J<Mq"-`EP"Sf+QZ(#bb6PrZRn*/&q<)8O1htk<G-cT(NV7(mh11FG7AGQ$.T!0mbqB>NGq:#$8aKR0hg0fP3*iX7#?5(4AK$)r^;;%KjW(NZ<Q$R?m[,d3kHo=0Bi)ET%fU3J3iEXh!5]@V[SU%G[)`%XLChILSl1O8A_1^f6)2mhmo85(Bl1u+*8(Oe!=jpGL6<qmh&*NJH1:57_eq2Ce<YGZdM0nZm.9\(f"hQY@#n+r1=SY_/#[BW6CK'dB9%iU;c;6c!>D1E)03:ol#UJ*=5d-#DuD8NYec]%X*5HIj\pK5!mB`]T+7h=6k"(9Bg1dK)CYmC>DB;W553RO3C,d@29G^R.*8Ge!=)g8m82DNo?NWLhLA-0GBS9_9iXFH2!,Ldg=_&F<25mc0b\e*nR\50C+(PLB+sPs4Y&0,fkaM/@0u&(#WV6,^J%-3/"ZT$mW"Jcrjbd1EN`E#gS&e8K-(\XOr<P=Z#,%/]o\?4QdpK?ni8$14<@T2I7drgX5rl&]ii@YTSbq7*'8Dtd.AK2%lhdAni8"*0V=1-D%Z$@N)r#\9<Z7-M3o,%3O>kl>$kFmaK#-ms"7WGebt3BrcYJA\oR1@-.d.)5<B7Yfmu!ub]3?c<`67ccZ41%Vn]p*.Asd<5\hrX7UCMEH)RfNk7Z'T?DmTXoXDC[P(hl%T^D7RO3L(Gfo^1Z0G@fupLu=K:HD^&B>o4aRM8bOfi]LVZ\i0O*%D`J2LV.0;;?.&-UDIuc@c)MH-F`>kl+TnLZIc,fQ5r`?584=O^4>Ma@k#U_=4*X42<2e+!^/_4>p:I7KNUk?204iacYK^*lq9N]eSRfc/'^b_jR,*QMi\.hm]'R-d%(0-FDKJbY=rLajaYW^t()!-4V@rZQV\>9WL]Mc:OaEYc>gd_L/je'F_A=#eF>\ZeC2DT+'VIk8GN,I(BZG-C0:%5^aLme""pH$4!V6Q<7]Dd7EHJPB?7`5,o_[r<]s/nVN=Do`C=fo3K&eEB1Z.OF5j"`L#^_)mJiceu4NH~>endstream
-endobj
-16 0 obj
-<<
-/Filter [ /ASCII85Decode /FlateDecode ] /Length 1376
->>
-stream
-Gau`SD,8nW&H88.0lhC\:sL5K[WeZ6M7\KLM$$BJV';%)S2&]+YMJl_&0_&Mc)tofSIc`>kr'q8l^;DBSseCC@>A/VoBo*KJ%IBo*5ZY2g]3(Up)^`a0QMM)HG`'iD\q8Gg"pX[&qa:U9IZL&1!)sS^Z9Nu:kk%H+9!OiLg],V*sQjXA+lV%\G2(JTm2#;'](9X+L!1+6%9]+8&lkLI,H']Ujo!_@E`=/)"8)R_'48oCej^rmdfq3hXC,dNZqI!n@3CRK)6SDXXCH"k@:5oAH)+$!)!@eQc/MgQ&S\]EmbK"OoDQqp\8,j2G7bZEQclPr&EK6K=lai.S)ngj#dfa.)?%b@UK6k9%8I^,o>/EZ:194Je.stc6UkFeY'2^V3f8jPUJiYPs8p(j!)UpUi/-hiB^J;mSo:%Tmkp0RSkm_0rl)H/m!`LQhE+,Z)Um3@rbI.)NcatNT@)T\$gkdA;Wr+YKc!EY=9%"m++`q5q^cF$q*eGbi2j#9=2*LiLV-]N4l!Ml2K6LgfM.h<io`mVW8Pig.N/ZB`!]<P+M8E`H#Cn,XF.0i`S#HdFsB(GH9X=D66Lo6_bQ8ZGFUEfd<f/Pe8+d8hO/.q$i8kkphWc2&k^PbX-uEs$TRPA5Q+OU;JiSdj4!h-)d8REFUE5\0-YJFrGBsqMg/pMi%(l')a#FQ('Q<5[p2#,"40Z*2SBl*WOjS4>Fu(^]?[\<+Jo^gS_W;W4OgmI<r&pG\^1KMW,M.cGkoL'Ee=-cMm>1m`2Afm<6aE+K9GP8A=:3s"pkJO*8]"dg'/3Lo=$PQp#;15Z'7t`_\#0fs*cM/*'iS>aaPSfk7<hY-eeNn8bg?Ks?+p'fok^JC^_7nlZM@Q<DCD6/;NBS;!NqF#bo7&_@\<q#9rkl-fD!9\e^je&bij;r_cr,$)5qnMKAbL4Fe@Ug[B[SSWo<^eQte?CF_rZkTfE<-hCP.&PaulAqM);?1d(dYO"pJ%&k(88OCJk*2i.D7*O="BiE-Q,=P#Jh8$\Cr8gqY_A+.!i-r2HTX0as$rO"$WHa[LFIYDl85>7Db/>jVK0<&0\sTZ1sK7&bXOPY!3Au9k[0XQIN>P[#ItY1g!Dd:WiIj$l;#tEc%/I,Fm1@"?oiRMI<eJW4g@$nX*):DX#!i2>9f>pMW<g9`d+:r:sbs!T]s-:WT\Y1BQ]./2%b$UH]9njTXrSF[=h]*@.jE'AGJY[%t8NJm76Au_]HHAmS:#'nak6T^PP\]-Jj/1Pa?=$95)W8)rcc.fl/\F2KhUjHH2pj?nd-&dD>VMjQPZ_GBSH3cY,G4;QL&qbV9u@G6;-YoIg<X/Pc/m8b=iH3i90%'lUW[2ck(?[#qW=V8mgNDXV?OHN+#a#qp<~>endstream
-endobj
-17 0 obj
-<<
-/Filter [ /ASCII85Decode /FlateDecode ] /Length 741
->>
-stream
-Gat=(@8EX`&H0nXR#kS$H?UdF]kA'DG[AX_B[mkO\]=qU(bF9M(MB'_1&^Q"d&Oe!WY#MuiULOB]ug&R`M_VKiuI!(!1Sa-p`0W<n@N!CJn#W#cE6+[OPLColdT>*dhuSo.2UQEE[F=2VEX7a@2f0&g$^V!cFho9e'N!_Ub-iQY`haTU['[0Ol5gP`$H!F)o\"=:"p6WKj`q`L+J&&6rM&l7\rE9^[&)dgp'm\A.$H%"\fYYVnuL"2C+cIh>\BjMs*c[)aR2_W>j1T2Sr9r?!UUG\,>s(gtllHaUR!C<f#2^qtd6HPfX/0>78=F(^g_(k)j?]_LSnE,*.iB&9hGSl>iYI]e&+\%55c<BWut!*=[Y;BQS50*1[T>o\i(#10NZC<3\'@7D]01'KVh[KnOR,_%1&eV<Pl"A&Hl=_-q]QIU%uAPVOF6A-I<&Pds.bR[6+Y/4T59@>iJH>+*D2EZ&;4NRu0r#*!&:)(dCcEHTOUPK[-'Pp_Fj<>)_]>ga8@5.H3\N)\C,!cD9E]?Tm/qIVBkHHF;eA&SVV=ISO&^E!7p#D86(GlM5tn2"N8:.J)\E,LAL_cA9p@'V=IcBb>sQc]'G3n\9!a\&qLZVsA4*Ss*RCNZh&k?Fdl(n",^1:o\fGIta7K*5BT7rX)sM.d6CF95,u_-D6S$Rn_!&5n,1;cEsaN498(Q/Ru\#mr+t^#FQe_Qk.)@9%H8Zd.E;6XQ1$2Vjoci;<[W?Z)D~>endstream
-endobj
-xref
-0 18
-0000000000 65535 f 
-0000000073 00000 n 
-0000000144 00000 n 
-0000000251 00000 n 
-0000000363 00000 n 
-0000000468 00000 n 
-0000000551 00000 n 
-0000000746 00000 n 
-0000000941 00000 n 
-0000001136 00000 n 
-0000001251 00000 n 
-0000001447 00000 n 
-0000001517 00000 n 
-0000001820 00000 n 
-0000001899 00000 n 
-0000003698 00000 n 
-0000005662 00000 n 
-0000007130 00000 n 
-trailer
-<<
-/ID 
-[<fd6ab3d24cb1ffe196ce8b7318a5cde2><fd6ab3d24cb1ffe196ce8b7318a5cde2>]
-% ReportLab generated PDF document -- digest (http://www.reportlab.com)
+# 3. INITIALIZE DISPLAY CANVAS
+st.markdown("<h1 class='glow-title'>⚡ NEO-NET GLOBAL // INTERFACE</h1>", unsafe_allow_html=True)
+st.markdown("<p style='font-family: monospace; color: #64748b;'>CLOUD REASONING ENGINE PIPELINE: ACTIVE</p>", unsafe_allow_html=True)
+st.divider()
 
-/Info 12 0 R
-/Root 11 0 R
-/Size 18
->>
-startxref
-7962
-%%EOF
+# Check for hidden cloud secret configuration
+if "GROQ_API_KEY" not in st.secrets:
+    st.error("❌ SYSTEM ERROR: Missing GROQ_API_KEY in cloud dashboard settings configuration.")
+    st.stop()
+
+# Initialize public server engine connection using hidden environment variable
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+
+if "cyber_history" not in st.session_state:
+    st.session_state.cyber_history = [
+        {"role": "assistant", "content": "🧠 **[NEO-NET GLOBAL CORE ACTIVE]** System is now completely live. Cloud routing pipeline established successfully."}
+    ]
+
+# Render chat logs
+for message in st.session_state.cyber_history:
+    avatar = "🔮" if message["role"] == "user" else "⚙️"
+    clean_display = message["content"]
+    if "</think>" in clean_display:
+        clean_display = clean_display.split("</think>")[-1].strip()
+    with st.chat_message(message["role"], avatar=avatar):
+        st.markdown(clean_display)
+
+# 4. CHAT ENTRY MATRIX (Side-by-side arrangement)
+injected_context = ""
+
+# Setup side-by-side positioning grid with weights
+col1, col2 = st.columns([0.05, 0.95])
+
+with col1:
+    # Minimal file picker mapping to a single isolated plus icon
+    uploaded_file = st.file_uploader("+", type=["txt", "py", "md"], label_visibility="collapsed")
+    if uploaded_file is not None:
+        try:
+            file_contents = uploaded_file.read().decode("utf-8")
+            st.toast(f"📥 {uploaded_file.name} context cached!", icon="✔️")
+            injected_context = f"\n\n[FILE DATA ({uploaded_file.name})]:\n```\n{file_contents}\n```\n"
+        except Exception as e:
+            st.error(f"Error: {e}")
+
+with col2:
+    # Set placeholder text to an empty string layout configuration
+    user_prompt = st.chat_input("")
+
+if user_prompt:
+    full_processed_prompt = user_prompt + injected_context if injected_context else user_prompt
+    with st.chat_message("user", avatar="🔮"):
+        st.markdown(f"**[TRANS_IN]:** {user_prompt}")
+    st.session_state.cyber_history.append({"role": "user", "content": full_processed_prompt})
+
+    with st.chat_message("assistant", avatar="⚙️"):
+        try:
+            # Empty placeholders to handle live stream text formatting split
+            think_container = st.empty()
+            answer_container = st.empty()
+            
+            def response_streamer():
+                try:
+                    stream = client.chat.completions.create(
+                        model='qwen/qwen3.6-27b',
+                        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.cyber_history],
+                        temperature=0.7,
+                        stream=True
+                    )
+                    
+                    in_think_block = False
+                    think_buffer = ""
+                    initial_buffer = ""
+                    buffer_limit = 15
+                    
+                    for chunk in stream:
+                        if not chunk.choices or len(chunk.choices) == 0:
+                            continue
+                            
+                        delta = chunk.choices.delta if hasattr(chunk.choices, 'delta') else chunk.choices
+                        content = getattr(delta, 'content', None)
+                        
+                        if content is None:
+                            continue
+                            
+                        if len(initial_buffer) < buffer_limit and not in_think_block and not think_buffer:
+                            initial_buffer += content
+                            if "<think>" in initial_buffer:
+                                in_think_block = True
+                                think_buffer = initial_buffer.replace("<think>", "").strip()
+                                initial_buffer = ""
+                            continue
+                        
+                        current_chunk = content if not initial_buffer else (initial_buffer + content)
+                        initial_buffer = ""
+                        
+                        if "<think>" in current_chunk:
+                            in_think_block = True
+                            current_chunk = current_chunk.replace("<think>", "")
+                        
+                        if "</think>" in current_chunk:
+                            in_think_block = False
+                            current_chunk = current_chunk.replace("</think>", "")
+                            
+                            if is_local_user:
+                                with st.expander("⚙️ [SYSTEM_LOG // REASONING_PROCESS]", expanded=False):
+                                    st.code(think_buffer.strip())
+                            think_container.empty()
+                            continue
+
+                        if in_think_block:
+                            think_buffer += current_chunk
+                            if is_local_user:
+                                think_container.markdown(f"🤖 *Thinking...*\n```text\n{think_buffer}\n```")
+                        else:
+                            yield current_chunk
+                            
+                                except Exception as stream_err:
+                    yield f"\n\n⚠️ [STREAM_INTERRUPTION]: Cloud stream encountered a pocket drop. Details: {str(stream_err)}"
+                            
+            full_reply = answer_container.write_stream(response_streamer())
+            st.session_state.cyber_history.append({"role": "assistant", "content": full_reply})
+        except Exception as e:
+            st.error(f"[SYSTEM_LAUNCH_ERROR]: Cloud connection dropped. Details: {e}")
